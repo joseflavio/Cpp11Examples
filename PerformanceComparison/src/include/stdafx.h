88e11460568bb6e73ab1dcc4f2c7c9620ce4da06
Jose Flavio A <joseflavio-at-gmail-dot-com>
2014-10-06 19:01:02 +0200

    Multiple Targets

--- PerformanceComparison/src/include/stdafx.h
+++ PerformanceComparison/src/include/stdafx.h
@@ -7,15 +7,21 @@
 #include <set>
 #include <map>
 
+#include <inttypes.h>
+
 #include <complex>
 
 #include <algorithm> // std::sort
 #include <fstream> // To read/write file streams: std::ifstream
 #include <iostream>
-#include <random>
 #include <sstream>
 #include <string> // For std::to_string
 #include <unistd.h> // sleep()
 #include <memory> // For Smart Pointers
+
+// C++11
+// #include <random>
+// #include <bitset>
+
 
 #endif /* STDAFX_H_ */
--- PerformanceComparison/src/src/Main.cpp
+++ PerformanceComparison/src/src/Main.cpp
@@ -3,29 +3,56 @@
 #include <Timer.hpp>
 #include <PrintUtils.hpp>
 
+
+class MyRandom {
+
+    //    std::random_device rd;
+    //    std::default_random_engine generator { rd() };
+    //    std::uniform_int_distribution<uint64_t> distribution;
+
+public:
+
+    MyRandom() {
+        srand(time(NULL));
+    }
+    // uint64_t generate() {
+    //    return distribution(generator);
+    // }
+
+    uint64_t generate() {
+        uint64_t hh = ((uint64_t) random() << 48) & 0xFFFF000000000000ull;
+        uint64_t hl = ((uint64_t) random() << 32) & 0x0000FFFF00000000ull;
+        uint64_t lh = ((uint64_t) random() << 16) & 0x00000000FFFF0000ull;
+        uint64_t ll = ((uint64_t) random()) & 0x000000000000FFFFull;
+        ;
+        return hh | hl | lh | ll;
+    }
+};
+
 void PerformanceTest() {
 
     static const size_t MAX_I = 50000;
     static const size_t MAX_J = 2000;
     static const size_t ELEMENT_SIZE = sizeof(uint64_t) + 40; // 20 bytes of container overhead
-    std::cout << "Expected memory needed: " << ( ELEMENT_SIZE * MAX_I * MAX_J ) / (1024 * 1024) << " MB" << std::endl;
+    std::cout << "Expected memory needed: "
+            << (ELEMENT_SIZE * MAX_I * MAX_J) / (1024 * 1024) << " MB"
+            << std::endl;
 
-    std::random_device rd;
-    std::default_random_engine generator { rd() };
-    std::uniform_int_distribution<uint64_t> distribution;
+    MyRandom rnd;
 
     Timer t1;
     {
         std::vector<std::set<uint64_t> > v;
-        for(size_t i = 0; i < MAX_I; ++i) {
+        for (size_t i = 0; i < MAX_I; ++i) {
             std::set<uint64_t> set;
-            for(size_t j = 0; j < MAX_J; ++j) {
-                uint64_t random_number = distribution(generator);
+            for (size_t j = 0; j < MAX_J; ++j) {
+                const uint64_t random_number = rnd.generate();
+                // std::bitset<64> bs(random_number); std::cout << random_number << " : " << bs << std::endl;
                 set.insert(random_number);
             }
             v.push_back(set);
         }
-        std::cout << "(construction) t1 elapsed = " << Timer::Elapsed(t1) << std::endl;
+        std::cout << "(construction) elapsed = " << Timer::Elapsed(t1) << std::endl;
 
         // std::cout << "sleeping..." << std::endl; sleep(10);
 
@@ -33,17 +60,16 @@ void PerformanceTest() {
         std::sort(v.begin(), v.end());
         std::cout << "(sort) t2 elapsed = " << Timer::Elapsed(t2) << std::endl;
 
-
         Timer t3;
-        std::rotate(v.begin(), (v.begin() + (v.size()/2)), v.end());
-        std::cout << "(rotate) t3 elapsed = " << Timer::Elapsed(t3) << std::endl;
+        std::rotate(v.begin(), (v.begin() + (v.size() / 2)), v.end());
+        std::cout << "(rotate) t3 elapsed = " << Timer::Elapsed(t3)
+                << std::endl;
 
         std::cout << "total elapsed = " << Timer::Elapsed(t1) << std::endl;
     }
     std::cout << "total elapsed = " << Timer::Elapsed(t1) << std::endl;
 
 }
-
 
 int main() {
 
