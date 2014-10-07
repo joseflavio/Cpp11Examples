1aa0b2fb8ed115657f78b9504cdb4debbf93c7ca
Jose Flavio A <joseflavio-at-gmail-dot-com>
2014-10-07 11:52:26 +0200

    Unifying compile scripts

---
+++ PerformanceComparison/compile.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+echo Compiling using Cpp03 and GCC 4.3.2
+/opt/gcc-4.3.2/bin/g++ -Werror -Wall -Wextra -I./src/include ./src/src/*.cpp -o ./build/pcomp_cpp03_gcc432.out
+echo done.
+
+echo Compiling using Cpp11 and GCC 4.9.1
+~seidelde/local/gcc/bin/g++ -std=c++11 -Werror -Wall -Wextra -I./src/include ./src/src/*.cpp -o ./build/pcomp_cpp11_gcc491.out
+echo done.
+
+
+echo Compiling using Cpp11 and Clang 3.5.0
+~seidelde/local/clang/bin/clang++ -std=c++11 -Werror -Wall -Wextra -I./src/include ./src/src/*.cpp -o ./build/pcomp_cpp11_clang350.out
+echo done.
+
+# Need for runtime? To be checked
+# export LD_LIBRARY_PATH="~seidelde/local/gcc/4.9.1/lib64"
+
--- PerformanceComparison/compile_clang350.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-if [ ! -d "build" ]; then
-    mkdir build
-fi
-
-export LD_LIBRARY_PATH="~seidelde/local/gcc/4.9.1/lib64"
-~seidelde/local/clang/bin/clang++ -std=c++11 -Werror -Wall -Wextra -I./src/include ./src/src/*.cpp -o build/Cpp11Examples_clang350.out
-
