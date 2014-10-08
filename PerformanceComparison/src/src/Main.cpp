547f23e0c03ce97c7a9606e94d50bf5fd06dc928
Jose Flavio A <joseflavio-at-gmail-dot-com>
2014-10-08 12:00:35 +0200

    Improving the test

--- PerformanceComparison/src/src/Main.cpp
+++ PerformanceComparison/src/src/Main.cpp
@@ -10,23 +10,24 @@ class MyRandom {
     //    std::default_random_engine generator { rd() };
     //    std::uniform_int_distribution<uint64_t> distribution;
 
+    // uint64_t generate() {
+    //    return distribution(generator);
+    // }
+
 public:
 
     MyRandom() {
         srand(time(NULL));
     }
-    // uint64_t generate() {
-    //    return distribution(generator);
-    // }
 
     uint64_t generate() {
         uint64_t hh = ((uint64_t) random() << 48) & 0xFFFF000000000000ull;
         uint64_t hl = ((uint64_t) random() << 32) & 0x0000FFFF00000000ull;
         uint64_t lh = ((uint64_t) random() << 16) & 0x00000000FFFF0000ull;
         uint64_t ll = ((uint64_t) random()) & 0x000000000000FFFFull;
-        ;
         return hh | hl | lh | ll;
     }
+
 };
 
 void PerformanceTest() {
@@ -35,8 +36,7 @@ void PerformanceTest() {
     static const size_t MAX_J = 1500;
     static const size_t ELEMENT_SIZE = sizeof(uint64_t) + 40; // 20 bytes of container overhead
     std::cout << "Expected memory needed: "
-            << (ELEMENT_SIZE * MAX_I * MAX_J) / (1024 * 1024) << " MB"
-            << std::endl;
+            << (ELEMENT_SIZE * MAX_I * MAX_J) / (1024 * 1024) << " MB\n";
 
     MyRandom rnd;
 
@@ -52,22 +52,21 @@ void PerformanceTest() {
             }
             v.push_back(set);
         }
-        std::cout << "(construction) elapsed = " << Timer::Elapsed(t1) << std::endl;
+        std::cout << "(construction) elapsed = " << Timer::Elapsed(t1) << "\n";
 
         // std::cout << "sleeping..." << std::endl; sleep(10);
 
         Timer t2;
         std::sort(v.begin(), v.end());
-        std::cout << "(sort) t2 elapsed = " << Timer::Elapsed(t2) << std::endl;
+        std::cout << "(sort) t2 elapsed = " << Timer::Elapsed(t2) << "\n";
 
         Timer t3;
         std::rotate(v.begin(), (v.begin() + (v.size() / 2)), v.end());
-        std::cout << "(rotate) t3 elapsed = " << Timer::Elapsed(t3)
-                << std::endl;
+        std::cout << "(rotate) t3 elapsed = " << Timer::Elapsed(t3) << "\n";
 
-        std::cout << "total elapsed = " << Timer::Elapsed(t1) << std::endl;
+        std::cout << "total elapsed = " << Timer::Elapsed(t1) << "\n";
     }
-    std::cout << "total elapsed = " << Timer::Elapsed(t1) << std::endl;
+    std::cout << "total elapsed = " << Timer::Elapsed(t1) << "\n";
 
 }
 
